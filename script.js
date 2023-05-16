a31761df68b0c0c5dac0b637fe38464050cbca26
Mariya Sharova <marii.savit@gmail.com>
2023-05-16 14:09:30 +0300

    fix file js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const resultClick = document.querySelector('.click-result');
 resultClick.addEventListener('click', () => alert('уря!'));
 
-
 const inputBlur = document.querySelector('.input-lose');
 inputBlur.onblur = () => alert('ждем возвращения')
 
-
 const inputValue = document.querySelector('.input-text');
 const buttonValue = document.querySelector('.button-text');
-
 buttonValue.addEventListener('click', () => {
     buttonValue.value = inputValue.value
 })
